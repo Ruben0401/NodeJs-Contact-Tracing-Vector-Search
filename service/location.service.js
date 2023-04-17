418c0f9a4fca7b73841eaf97aa294ade87e6ac1b
Ruben0401 <rubencio_123@hotmail.com>
2023-04-16 22:50:21 -0500

    fix 0

--- service/location.service.js
+++ service/location.service.js
@@ -1,7 +1,7 @@
 const { upsertLocation } = require('../repository/pinecone.repository');
 
 const updateLocation = async (userId, latitude, longitude,contagiado) => {
-  return await upsertLocation(userId, [latitude, longitude],contagiado,[latitude*10000, longitude*10000]);
+  return await upsertLocation(userId, [latitude, longitude],contagiado,[latitude*100000, longitude*100000]);
 };
 
 module.exports = {
