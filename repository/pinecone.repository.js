f9e1d1fefb97e4507a429ba704748bf27d408e5c
Ruben0401 <rubencio_123@hotmail.com>
2023-04-16 22:25:15 -0500

    filter id and distance

--- repository/pinecone.repository.js
+++ repository/pinecone.repository.js
@@ -9,19 +9,23 @@ const upsertLocation = async (userId, location,contagiado) => {
         values: location,
         metadata: {
           userId,
-          contagiado
+          contagiado,
         },
       },
     ],
     namespace:PINECONE_NAMESPACE
   };
-  const nearestUsers = await findNearestUsers(location, 10);
+  let nearestUsers = [];
+  if(!contagiado){
+     nearestUsers = await findNearestUsers(location, 10,userId);
+    
+  }
   await pineconeInstance.index.upsert({ upsertRequest });
 
   return nearestUsers;
 };
 
-const findNearestUsers = async (vector, topK) => {
+const findNearestUsers = async (vector, topK,id) => {
   const queryRequest = {
     vector,
     topK,
@@ -35,8 +39,27 @@ const findNearestUsers = async (vector, topK) => {
   const searchResponse = await pineconeInstance.index.query({
     queryRequest,
   });
-  return searchResponse;
+  const matches = searchResponse["matches"]
+  const final =[]
+  for (let i=0;i<matches.length;i++){
+    let obj = {...matches[i]}
+    if(obj["id"] ===  id ) continue;
+    let distance = calcularDistancia(vector[0],vector[1],obj.values[0],obj.values[1])
+    obj["distance"] = distance
+    final.push(obj)
+  }
+  return final;
 };
+
+function calcularDistancia(lat1, lon1, lat2, lon2) {
+  const R = 6371000; // radio de la tierra en metros
+  const dLat = (lat2 - lat1) * Math.PI / 180;
+  const dLon = (lon2 - lon1) * Math.PI / 180;
+  const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLon/2) * Math.sin(dLon/2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  const distancia = R * c;
+  return distancia;
+}
 
 module.exports = {
   upsertLocation,
